77d77d12f89b592c605acb794fc9aa854a69b676
Kevin Perez <unknotted26@gmail.com>
2016-07-12 21:49:33 -0400

    added functions to class TodoList

--- todolist.rb
+++ todolist.rb
@@ -6,7 +6,7 @@ At least one method that returns a boolean (ex. def completed?).
 Print methods for each of the classes that create a nicely formatted to-do list format.
 =end
 class TodoList
-  attr_reader :title, :items
+  attr_accessor :title, :items
 
   def initialize(listTitle)
     @title = listTitle
@@ -19,19 +19,37 @@ class TodoList
   end
 
   def display_list
-    puts "List title: #{title}"
-    puts "List items:"
+    puts "---------------"
+    puts " #{title}"
+    puts "---------------"
+    itemCount = 1
     items.each do |item|
-      puts "  Description: #{item.description}"
-      puts "  Completed?   #{item.completion_status}"
+      print "#{itemCount}."
+      puts " Description: #{item.description}"
+      puts "   Completed?   #{item.completion_status}"
+      itemCount = itemCount + 1
     end
+  end
+
+  def delete_first_item
+    items.delete_at(0)
+  end
+
+  def item_completed(item_num)
+    items[item_num].completion_status = true
+  end
+
+  def update_title(newTitle)
+    @title = newTitle
   end
 end
 
 class Item
-  attr_reader :description, :completion_status
+  attr_accessor :description, :completion_status
   def initialize(item_description)
     @description = item_description
     @completion_status = false
   end
+
+
 end
--- udacitask.rb
+++ udacitask.rb
@@ -4,20 +4,32 @@ require_relative 'todolist.rb'
 list = TodoList.new("List1")
 # Add four new items
 list.add_item("item1")
+list.add_item("Do Homework")
+list.add_item("Do Chores")
+list.add_item("Pick up Jimmy")
+
 # Print the list
 list.display_list
+
 # Delete the first item
+list.delete_first_item
 
 # Print the list
+list.display_list
 
 # Delete the second item
-
+list.delete_first_item
 # Print the list
+list.display_list
 
 # Update the completion status of the first item to complete
+list.item_completed(0)
 
 # Print the list
+list.display_list
 
 # Update the title of the list
+list.update_title("New List Title")
 
 # Print the list
+list.display_list
